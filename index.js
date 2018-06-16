71d67eeb6084493c46d2d2bca2e3a2be8a9401b1
Nicolas Drouard <nicolasdrouard19@gmail.com>
2018-06-16 19:47:03 +0100

    read write file

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+var fs = require('fs');
+
 function sayHello(name) {
   console.log('hello ' + name);
 }
 
+function CreateFile() {
+  fs.appendFile('mynewfile1.txt', 'Hello content yolo!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+}
+
+function ReadFile() {
+  fs.readFile('mynewfile1.txt', 'utf8', function(err, data) {
+    console.log(data);
+    });
+}
+
 sayHello('yolo v0001');
+CreateFile();
+ReadFile();
